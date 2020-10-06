fd73ea349d23393e80e80b7459b10ce3f4752a00
tronsage <tronsage1@gmail.com>
2020-10-06 13:34:46 +0100

    new changes

--- index.js
+++ index.js
@@ -12,3 +12,99 @@ closeBtn.onclick = function(){
   closeBtn.style.display = 'none'
   menuBtn.style.display = 'block'
 }
+
+
+var joinBtn = document.getElementById('joinBtn')
+var loginBtn = document.getElementById('loginBtn')
+var openCon = document.getElementById('join_con')
+var con = document.getElementById('con')
+var rFeeBtn = document.getElementById('rFee')
+
+joinBtn.onclick = function(){
+  openCon.style.display = 'block'
+  con.style.display = 'none'
+}
+rFeeBtn.onclick = function(){
+  openCon.style.display = 'block'
+  con.style.display = 'none'
+}
+
+
+
+
+var walletBtn = document.getElementById('wallet_icon')
+var showWallet = document.getElementById('wallet_con_wrap')
+var closeWallet = document.getElementById('close_wallet')
+
+walletBtn.onclick = function(){
+  showWallet.style.display = 'block'
+}
+closeWallet.onclick = function(){
+  showWallet.style.display = 'none'
+}
+
+var lgin = document.getElementById('lgin')
+var cwbtn = document.getElementById('cwbtn')
+var showlogin_con = document.getElementById('login_con')
+var create_wallet = document.getElementById('create_wallet')
+
+lgin.onclick = function(){
+  showlogin_con.style.display = 'block'
+  create_wallet.style.display = 'none'
+}
+cwbtn.onclick = function(){
+  showlogin_con.style.display = 'none'
+  create_wallet.style.display = 'block'
+}
+
+
+
+
+var lcs_Btn =document.getElementById('lc_submit')
+var cw_Btn = document.getElementById('c_wallet')
+var show_wmain = document.getElementById('w_main')
+
+lcs_Btn.onclick = function(){
+  show_wmain.style.display = 'block'
+  showlogin_con.style.display = 'none'
+}
+cw_Btn.onclick = function(){
+  show_wmain.style.display = 'block'
+  create_wallet.style.display = 'none'
+}
+
+
+
+var sendBtn = document.getElementById('send_trx')
+var receiveBtn = document.getElementById('receive_trx')
+var sendCon = document.getElementById('send_con')
+var receiveCon = document.getElementById('recieve_con')
+
+sendBtn.onclick = function(){
+  sendCon.style.display = 'block'
+  show_wmain.style.display = 'none'
+}
+receiveBtn.onclick = function(){
+  receiveCon.style.display = 'block'
+  show_wmain.style.display = 'none'
+}
+
+var scBack = document.getElementById('sc_back')
+scBack.onclick = function(){
+  sendCon.style.display = 'none'
+  show_wmain.style.display = 'block'
+}
+var rcBack = document.getElementById('rc_back')
+rcBack.onclick = function(){
+  receiveCon.style.display = 'none'
+  show_wmain.style.display = 'block'
+}
+
+
+
+var sendBtn = document.getElementById('send_btn')
+var ibText = document.getElementById('insuficient_fund')
+
+sendBtn.onclick = function(){
+  ibText.style.display = 'block'
+}
